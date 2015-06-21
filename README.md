3fcedc7757cf3697661da1cca872e0f37ea89122
Jesus Alfonso Moreno <ing.jesusmoreno@gmail.com>
2015-06-21 01:30:29 -0500

    last version of script

--- README.md
+++ README.md
@@ -6,16 +6,32 @@ output: html_document
 
 # Getting Data Project
 
-This scripts performs the following steps in the same order:
+## Introduction
+
+The script in general performs the following steps:
 
 1. Merges the training and the test sets to create one data set.
-2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
 3. Uses descriptive activity names to name the activities in the data set
 4. Appropriately labels the data set with descriptive variable names.
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+As indicated in the project instructions for Coursera's Getting and Cleaning Data cousera.
     
-## Prerequisites
+## Prerequisites for running the scripts
 
 In order to properly run the script, ensure that the following file is unzipped in the R working directory of the session: <https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip>
+
+If there's an internet connection available, the script will try to download the zipped file. Else ensure that the proper file is in the root of the working directiry. 
+
+Also the script uses the reshape2 library. If unavailable use the following code to install:
+
+```{r}
+install.packages("reshape2",dependencies = c("Depends", "Imports", "LinkingTo", "Suggests", "Enhances"))
+```
+
+Last but not least, R must installed in order to run the code.
+
 
 ## Analysis
 
@@ -23,37 +39,53 @@ In order to properly run the script, ensure that the following file is unzipped 
 
 The former link contains Test and Train data sets for a machine learning research project, using cellphone's sensors to identify the activities performed by the test subjects. The first step involves loading both X (independent variables) data sets into two data frames.
 
-Since both data sets lack a header row with column names, the script is hardcoded to provide such variable labels. Columns names are based on the "features.txt", which is insde the "UCI HAR Dataset" folder. Also while loading the data sets, unwated columns are discarted.
+Since both data sets lack a header row with column names, the script is hardcoded to provide such variable labels when loading both tables. Columns names are based on the "features.txt", which is insde the "UCI HAR Dataset" folder. Also while loading the data sets, unwated columns are discarted.
+
 
 The second step involves loading the train and test Y data sets. Both Y data sets contain a vector with the activities ID performed by the subjects. Each row of the vector corresponds to the same row of the X dataframe.
 
 The third data load is the subject list of each case. This data can be found in the "subject_test.txt" file of each data case. The file contains a vector with subject's ID, since no descriptions are needed it will be fine as it is.
 
-Last, load the data set containing activities descriptions. Since both test and train data share the same codes, one data load will sufice this time. 
+then the scripts load the data set containing activities descriptions. Since both test and train data share the same codes, one data load will sufice this time. 
 
 
-### concatenating tables
+### concatenating and mergingtables
+
 The following concatenation procedures are performed in the order presented:
 
-1. X Train Data Frame with Y Train Data Frame (columns wise)
-2. X Test Data Frame with Y Test Data Frame (columns wise)
-3. subject_test vector with the result of concatenate 1 (column wise)
-4. subject_test vector with the result of concatenate 2 (column wise)
-5. Result of the 3rd concatenation with result of the 4th concatenation (rows wise). From now on this result will be called **Complete Table**
+1. X Train Data Frame, with Subjects ID Table and Activities ID table (columns wise). The resulting data frame is named **train**.
+2. X Test Data Frame, with Test Subjects ID Table and Test Activities ID table (columns wise). The resulting data frame is named **test**.
+3. Concatenate **train and test** dataframes, rows wise. The resulting data framed is named **complete**
+4. Merge **complete** data frame with the activities label data frame, by activities id column.
 
+### Removing unwanted columns
 
-### Merging tables
-
-Since Y data sets only contain codes, it's important to add descriptions to make the data frame more understandable for humans. To achieve this the script uses the merge function.
+All unneeded columns are removed, leaving only 66 variables.
 
 
 ### Grouping/pivoting data
 
+The reshape function is used. This involves 3 steps:
 
+1. Identifying all numerical variables (non label/ID columns. To simplify the process when loading the data, all non key columns where assigned a double class. While key columns where assigned a character or integer class. The class serves as a flag to discriminate variables. The following code is used to create a vector with column's name that contain amounts:
 
 
 ```{r}
-summary(cars)
+vValuesCol = names(complete[,sapply(complete, is.double)])
 ```
 
+2. Defining an intermediate data frame to enable grouping. Reshape2 library contains the melt function which outputs a DF with a clear distinction between ID variables and amount columns. The following code was used:
 
+```{r}
+dfmComplete = melt(complete,id=c("Activities_Label","Subjects_ID"),measure.vars = vValuesCol)
+```
+3. Defining the resulting group table, by using Reshape2's dcast function:
+
+```{r}
+dfcComplete = dcast(dfmComplete, Activities_Label + Subjects_ID ~ variable,mean)
+```
+
+Last but not least the dfcComplete table is saved in the working directory, using pipes "|" seperators:
+```{r}
+write.table(dfcComplete,file = "reshapedDataActivitiesSubjects",row.names = F,sep="|")
+```
--- run_analysis.R
+++ run_analysis.R
@@ -2,41 +2,85 @@
 library(reshape2)
 #verifies if UCI HAR Dataset exits, if not it downloads and unzip the file
 
-###loads all files needed
-#loads col names
-colNames = read.table("completeVariableDesc.vector")
-vColNames = as.vector(colNames$V1) #makes sure it's a vector
-#loads train independent data
-x_train_complete = read.table("UCI HAR Dataset\\train\\X_train.txt",col.names = vColNames,colClasses = "double")
-#loads test independent data
-x_test_complete = read.table("UCI HAR Dataset\\test\\X_test.txt",col.names = vColNames,colClasses = "double")
-#loads activities (Y variabe)
-y_train = read.table("UCI HAR Dataset\\train\\y_train.txt",header=F,col.names = "Activities_ID",colClasses = "integer")
-y_test = read.table("UCI HAR Dataset\\test\\y_test.txt",header=F,col.names = "Activities_ID")
-#loads activities descriptions
-y_labels = read.table("UCI HAR Dataset\\activity_labels.txt",header=F,col.names =c("Activities_ID" ,"Activities_Label"),colClasses = c("integer","character"))
-#loads subjects data
-subjects_train = read.table("UCI HAR Dataset\\train\\subject_train.txt",header=F,col.names = "Subjects_ID",colClasses = "integer")
-subjects_test = read.table("UCI HAR Dataset\\test\\subject_test.txt",header=F,col.names = "Subjects_ID",colClasses = "integer")
+checksDir = function(sPath,url,rootFile){
+  
+  if (!file.exists(sPath)){
+    print(paste(sPath," does not exists, trying to retrieve it from web."))
+    #downloads zip file with data
+    download.file(url,destfile = paste(rootFile,".zip",sep = ""),method = "auto")
+    #renives recursively incomplete data dir
+    unlink(rootFile,recursive = T)
+    #unzips file
+    unzip(paste(rootFile,".zip",sep = ""))
+    unlink(paste(rootFile,".zip",sep = ""))
+  } 
+  
+}
 
-#concatenates x data, y data, subject data and activities id data
-train = cbind(x_train_complete,subjects_train,y_train)
-test = cbind(x_test_complete,subjects_test,y_test)
+main = function(){ #main function, where the magic happens
+  
+  #data directory constant
+  subDir = "UCI HAR Dataset"
+  url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  
+  checksDir(subDir,url,subDir)
+  
+  ###loads all files needed
+  #loads col names
+  colNames = read.table("completeVariableDesc.vector")
+  vColNames = as.vector(colNames$V1) #makes sure it's a vector
+  #loads train independent data
+  sPath = paste(subDir,"\\train\\X_train.txt",sep = "")
+  checksDir(sPath,url,subDir)
+  x_train_complete = read.table(sPath,col.names = vColNames,colClasses = "double")
+  #loads test independent data
+  sPath = paste(subDir,"\\test\\X_test.txt",sep = "")
+  checksDir(sPath,url,subDir)
+  x_test_complete = read.table(sPath,col.names = vColNames,colClasses = "double")
+  #loads activities (Y variabe)
+  sPath = paste(subDir,"\\train\\y_train.txt",sep = "")
+  checksDir(sPath,url,subDir)
+  y_train = read.table(sPath,header=F,col.names = "Activities_ID",colClasses = "integer")
+  sPath = paste(subDir,"\\test\\y_test.txt",sep = "")
+  checksDir(sPath,url,subDir)
+  y_test = read.table(sPath,header=F,col.names = "Activities_ID")
+  #loads activities descriptions
+  sPath = paste(subDir,"\\activity_labels.txt",sep = "")
+  checksDir(sPath,url,subDir)  
+  y_labels = read.table(sPath,header=F,col.names =c("Activities_ID" ,"Activities_Label"),colClasses = c("integer","character"))
+  #loads subjects data
+  sPath = paste(subDir,"\\train\\subject_train.txt",sep = "")
+  checksDir(sPath,url,subDir)  
+  subjects_train = read.table(sPath,header=F,col.names = "Subjects_ID",colClasses = "integer")
+  sPath = paste(subDir,"\\test\\subject_test.txt",sep = "")
+  checksDir(sPath,url,subDir)  
+  subjects_test = read.table(sPath,header=F,col.names = "Subjects_ID",colClasses = "integer")
+  
+  #concatenates x data, y data, subject data and activities id data
+  train = cbind(x_train_complete,subjects_train,y_train)
+  test = cbind(x_test_complete,subjects_test,y_test)
+  
+  #concatenates train and test data
+  
+  complete = rbind(train,test,deparse.level = 1)
+  
+  #drops all unwanted columns
+  complete = complete[,c(1,2,3,41,42,43,81,82,83,121,122,123,161,162,163,201,214,227,240,253,266,267,268,345,346,347,424,425,426,503,516,529,542,4,5,6,44,45,46,84,85,86,124,125,126,164,165,166,202,215,228,241,254,269,270,271,348,349,350,427,428,429,504,517,530,543,562,563)]
+  
+  
+  
+  #merges acttivities name to ensure that the dataset is easy to understand
+  complete = merge.data.frame(complete,y_labels,by = "Activities_ID")
+  #create vector with value columns
+  vValuesCol = names(complete[,sapply(complete, is.double)])
+  #groups complete table by activity label and subject id, with an average function
+  dfmComplete = melt(complete,id=c("Activities_Label","Subjects_ID"),measure.vars = vValuesCol)
+  dfcComplete = dcast(dfmComplete, Activities_Label + Subjects_ID ~ variable,mean)
+  #writes table to dir
+  write.table(dfcComplete,file = "reshapedDataActivitiesSubjects",row.names = F,sep="|")
+  print("Script finished")
+}
 
-#concatenates train and test data
-
-complete = rbind(train,test,deparse.level = 1)
-
-#drops all unwanted columns
-complete = complete[,c(1,2,3,41,42,43,81,82,83,121,122,123,161,162,163,201,214,227,240,253,266,267,268,345,346,347,424,425,426,503,516,529,542,4,5,6,44,45,46,84,85,86,124,125,126,164,165,166,202,215,228,241,254,269,270,271,348,349,350,427,428,429,504,517,530,543,562,563)]
 
 
-
-#merges acttivities name to ensure that the dataset is easy to understand
-complete = merge.data.frame(complete,y_labels,by = "Activities_ID")
-
-#groups complete table by activity label and subject id, with an average function
-dfmComplete = melt(complete,id=c("Activities_Label","Subjects_ID"),measure.vars = vValuesCol)
-dfcComplete = dcast(dfmComplete, Activities_Label + Subjects_ID ~ variable,mean)
-#writes table to dir
-write.table(dfcComplete,file = "reshapedDataActivitiesSubjects",row.names = F)
+main() #starts script
